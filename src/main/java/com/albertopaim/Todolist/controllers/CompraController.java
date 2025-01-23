f5b08ed572db38a762d5fb799c322f33e45c8dcd
Alberto Paim <albertoopaim@gmail.com>
2025-01-23 19:40:31 -0300

    "retomando"

--- src/main/java/com/albertopaim/Todolist/controllers/CompraController.java
+++ src/main/java/com/albertopaim/Todolist/controllers/CompraController.java
@@ -2,15 +2,13 @@ package com.albertopaim.Todolist.controllers;
 
 import com.albertopaim.Todolist.Dtos.CompraDTO;
 import com.albertopaim.Todolist.model.Compra;
-import com.albertopaim.Todolist.repositories.CompraRepository;
 import com.albertopaim.Todolist.services.CompraService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/compra")
@@ -19,77 +17,18 @@ public class CompraController {
     @Autowired
     private CompraService compraService;
 
-    @Autowired
-    private CompraRepository compraRepository;
-
     @PostMapping
     public ResponseEntity<CompraController> createCompra(@RequestBody CompraDTO compraDTO) {
-
         compraService.create(compraDTO);
-
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @GetMapping()
-    public ResponseEntity<List<CompraDTO>> getCompra() {
+    @GetMapping
+    public ResponseEntity<List<Compra>> getCompra(){
 
+        List listaDto = resultado.stream().map(autor -> new AutorDTO(autor.getId(), autor.getName(), autor.getDataNascimento(), autor.getNacionalidade())).collect(Collectors.toList())
         List<Compra> resultado = compraService.listarCompras();
-
-        List<CompraDTO> compras = new ArrayList<>();
-
-        for (Compra compra : resultado) {
-            CompraDTO compraDTO = new CompraDTO(compra.getNome(), compra.getDataCompra());
-
-            compras.add(compraDTO);
-        }
-        return ResponseEntity.status(HttpStatus.OK).body(compras);
+        return resultado;
     }
 
-    @GetMapping("{id}")
-    public ResponseEntity<CompraDTO> getCompraId(@PathVariable String id) throws Exception {
-
-        var uuid = UUID.fromString(id);
-        Optional<Compra> compraEncontrada = compraRepository.findCompraById(uuid);
-
-        if (compraEncontrada.isEmpty()) {
-            throw new Exception("Compra não encontrada");
-        } else {
-            Compra compra = compraEncontrada.get();
-            CompraDTO compraDTO = new CompraDTO(compra.getNome(), compra.getDataCompra());
-            return ResponseEntity.status(HttpStatus.OK).body(compraDTO);
-        }
-
-    }
-
-
-    @PutMapping("{id}")
-    public ResponseEntity<Object> update(@PathVariable String id, @RequestBody CompraDTO compraDTO) throws Exception {
-
-        var uuid = UUID.fromString(id);
-
-        Optional<Compra> compraEncontrada = compraRepository.findCompraById(uuid);
-
-        if (compraEncontrada.isPresent()) {
-
-            Compra compra = compraEncontrada.get();
-            compra.setNome(compraDTO.nome());
-            compra.setDataCompra(compraDTO.dataCompra());
-
-            compraService.updateCompra(compra);
-
-            return ResponseEntity.status(HttpStatus.OK).body(compraEncontrada);
-
-        } else {
-            throw new Exception("Compra não encontrada");
-        }
-
-    }
-
-    @DeleteMapping("{id}")
-    public ResponseEntity deletar(@PathVariable String id) throws Exception {
-        var uuid = UUID.fromString(id);
-        compraService.deletar(uuid);
-
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-    }
 }
